525bcdf7b698066afbce88996f67c2a978b0913e
Dr. Merge <eccretav@gmail.com>
2023-05-13 07:34:34 -0500

    Changed the url

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const engine = require("./engine");
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public"))); // add this line to serve static files
 engine();
-app.get("/sapp", async (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "qr.png"));
+app.get("/qrcode", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "qr.png")); // update this line to send the file
 });
 
 app.listen(PORT, () => {
